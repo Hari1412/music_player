876de9bb2bfb06dc2553ffb7c5bf29b16b28521e
Hari1412 <145188851+Hari1412@users.noreply.github.com>
2023-10-21 12:28:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,111 @@
+let song_image = document.getElementById("cover_photo")
+let song_title = document.getElementById("title")
+let song_singer = document.getElementById("singer")
+let song_play = document.getElementById("play")
+let song_prev = document.getElementById("prev")
+let song_next = document.getElementById("next")
+ 
+ 
+ 
+console.log(song_play)
+ 
+ 
+//creating song object list
+let songs_list = [
+    {
+        name : 'Bloody Sweet',
+        Image : 'bool.png',
+        Song : 'BloodySweet.mp3',
+        Singer : 'Ani'
+    },
+    {
+        name : 'Believer',
+        Image : 'bel.jpg',
+        Song : 'Believer.mp3',
+        Singer : 'Imagine Dragons'
+    },
+    {
+        name : 'Badass',
+        Image : 'badass.jpg',
+        Song : 'Badass.mp3',
+        Singer : 'Ani'
+    },
+    {
+        name : 'Villain Yaaru',
+        Image : 'villain.jpg',
+        Song : 'Villain-Yaaru.mp3',
+        Singer : 'Ani'
+    },
+    
+]
+ 
+
+let i=0;
+let flag=false;
+ 
+ 
+var audio = new Audio(songs_list[i].Song);
+song_image.src = songs_list[i].Image
+song_title.innerHTML = songs_list[i].name
+song_singer.innerHTML = songs_list[i].Singer
+ 
+
+song_play.addEventListener("click", function(){
+ 
+    if(flag ===false){
+        audio.play()
+        flag=true;
+        console.log(flag)
+    }
+    else{
+        audio.pause()
+        flag=false;
+        console.log(flag)
+    }
+})
+ 
+
+song_next.addEventListener("click", function(){
+   
+    audio.pause()
+    flag = false;
+    i = i+1;
+    if(i>=songs_list.length){
+       
+        i=0;
+    }
+    console.log(i)
+    song_image.src = songs_list[i].Image
+    song_singer.innerHTML = songs_list[i].Singer
+    song_title.innerHTML = songs_list[i].name
+    audio = new Audio(songs_list[i].Song)
+    if(flag ===false){
+        audio.play()
+        flag=true;
+        console.log(flag)
+    }
+   
+})
+ 
+
+song_prev.addEventListener("click", function(){
+   
+    audio.pause()
+    flag = false;
+    i = i-1;
+    if(i<=0){
+        i=songs_list.length-1;
+        console.log(i)
+    }
+    song_image.src = songs_list[i].Image
+    song_singer.innerHTML = songs_list[i].Singer
+    song_title.innerHTML = songs_list[i].name
+    audio = new Audio(songs_list[i].Song)
+    if(flag ===false){
+        audio.play()
+        flag=true;
+        console.log(flag)
+    }
+   
+   
+})
